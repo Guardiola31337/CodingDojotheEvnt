a4e267e2b5c4a07e37a6e2359f680c7e22549b1c
Pedro Duran <pedro.duranc@gmail.com>
2015-04-18 12:00:08 +0200

    First iteration

---
+++ gonzalo_pedro/app/src/androidTest/java/com/peshe/coding_dojo/codingdojo/ApplicationTest.java
@@ -0,0 +1,17 @@
+package com.peshe.coding_dojo.codingdojo;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+import android.util.Log;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+        Log.d("PEDRO_Test","Prueba de test");
+    }
+
+
+}
